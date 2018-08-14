405bda61faeb1a87f4204ef3c653c28bbd499022
Charandas Batra-Daitch <charandas108@gmail.com>
2018-08-14 11:30:04 -0400

    Remove local path

--- test/some-test.spec.ts
+++ test/some-test.spec.ts
@@ -11,7 +11,7 @@ describe('test lib', () => {
 /*
   ● Test suite failed to run
 
-    /Users/charandas/serve/asurity/RiskExec/depd-repro-jest/packages/some-esm-package/node_modules/depd/index.js:1
+    /some/dir/depd-repro-jest/packages/some-esm-package/node_modules/depd/index.js:1
     TypeError: callSite.getFileName is not a function
 
       at callSiteLocation (packages/some-esm-package/node_modules/depd/index.js:252:23)
